314e2e5b597402b89b92c05b1883d4e2f4223575
AdrianaHY <adriana.herya@gmail.com>
2017-03-23 15:10:40 -0600

    segundo avance

--- calculadora.js
+++ calculadora.js
@@ -1,4 +1,4 @@
-//Para mostrar los números
+//Para mostrar los números con click
   function mostrarNumero(num){
     var guardarNumero = document.formulario.resultado.value;
       document.formulario.resultado.value = guardarNumero + num;
@@ -10,4 +10,13 @@
 
   }
 
-//Para validar los signos
+//Para mostrar los números con la tecla presionada
+function validarNumero (event){
+  var ascci = event.keyCode;
+  if (ascci == 13){
+    operaciones();
+  }
+  if(ascci < 42 || ascci > 57){
+    return false;
+  }
+}
